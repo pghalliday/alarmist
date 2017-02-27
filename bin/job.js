2055896244f702b6f9c09f4a8b5398e6c344681c
Peter Halliday <pghalliday@gmail.com>
2017-02-27 02:15:33 +0100

    need shebang in bin scripts

--- bin/job.js
+++ bin/job.js
@@ -1,2 +1,3 @@
+#!/usr/bin/env node
 require('babel-polyfill');
 require('../lib/cli/job.js')(process.argv.slice(2));
--- bin/monitor.js
+++ bin/monitor.js
@@ -1,2 +1,3 @@
+#!/usr/bin/env node
 require('babel-polyfill');
 require('../lib/cli/monitor.js')(process.argv.slice(2));
